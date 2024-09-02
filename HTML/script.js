95e0be9d54c510874bad2dbe124a9b596ba0d577
MUHAMMAD ARFAN <106101890+arfan4-dev@users.noreply.github.com>
2024-09-02 16:42:51 +0500

    HTML

---
+++ HTML/script.js
@@ -0,0 +1,16 @@
+var circle = document.getElementById("circle");
+var btnUp = document.getElementById("btnUp");
+var btnDown = document.getElementById("btnDown");
+var rotateValue = circle.style.transform;
+var rotateSum;
+btnUp.onclick = function () {
+    rotateSum = rotateValue + "rotate(-90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+}
+
+btnDown.onclick = function () {
+    rotateSum = rotateValue + "rotate(90deg)";
+    circle.style.transform = rotateSum;
+    rotateValue = rotateSum;
+}
